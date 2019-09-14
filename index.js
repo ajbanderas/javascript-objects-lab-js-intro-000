a9b69a3169b060bc979c32408475d597df1af3dd
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-14 02:20:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {eggs: '3'}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newRecipes = Object.assign({},object,{[key]: value})
+  return newRecipes
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
