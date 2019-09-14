a3af53545e100b64f37f8956b62a20cf6976384d
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-14 03:42:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-function delete
+function deleteFromObjectByKey(object, key){
+  var newObject = Object.assign({}, object)
+  delete newObject[key]
+  return newObject
+}
